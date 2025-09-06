ac4326b65a93fe0d35d58f618063dcece77912dd
agusgdev2025 <AgustinNicolas.Guerrera@alumnos.uai.edu.ar>
2025-09-06 18:16:48 -0300

    Add files via upload

---
+++ TPLaboratorioUAI/frmForm1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPLaboratorioUAI
+{
+    public partial class frmForm1 : Form
+    {
+
+        public frmForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdbDni.Checked = true;
+            rdbHombre.Checked = true;
+            dtpFecNacimiento.MinDate = DateTime.Now.AddYears(-120);
+            dtpFecNacimiento.MaxDate = DateTime.Now.Date;
+            dtpDobleBacUltimaDosis.MinDate = DateTime.Now.AddYears(-15);
+            dtpDobleBacUltimaDosis.MaxDate = DateTime.Now.Date;
+            cboCovidDosis.SelectedIndex = 0;
+            cboCovidDosis.Enabled = false;
+            dtpDobleBacUltimaDosis.Enabled = false;
+            txtNombre.Focus();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            lstVacunacion.Items.Add($" Nombre y Apellido:  {txtNombre.Text}, {txtApellido.Text}");
+            lstVacunacion.Items.Add($" Nro. Documento:  {ObtenerTipoDocumento()} {txtNroDocumento.Text}");
+            lstVacunacion.Items.Add($" Fecha de Nacimiento:  {dtpFecNacimiento.Value.ToShortDateString()}");
+            lstVacunacion.Items.Add($" Localidad:  {cboLocalidad.Text}");
+            lstVacunacion.Items.Add($" Género:  {ObtenerGenero()}");
+            lstVacunacion.Items.Add("");
+            lstVacunacion.Items.Add(" Vacunas Aplicadas:  ");
+
+            string vacuna = string.Empty;
+
+            foreach (Control control in gbxVacunas.Controls)
+            {
+                if (control is CheckBox checkBox && checkBox.Checked) 
+                {
+                    vacuna = $"      - {checkBox.Text}";
+
+                    if (checkBox.Name == "chkCovid" && checkBox.Checked)
+                        vacuna += $"  ( Dosis: {cboCovidDosis.Text} )";
+                    
+                    if (checkBox.Name == "chkDobreBacteriana" && checkBox.Checked)
+                        vacuna += $"  ( Última Dosis: {dtpDobleBacUltimaDosis.Value.ToShortDateString()} )";
+
+                    lstVacunacion.Items.Add(vacuna);
+                }
+            }
+
+            lstVacunacion.Items.Add("------------------------------------------------------------");
+            MessageBox.Show("Registro de vacunas completado con éxito.", "Registración de vacunas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            RestablecerControles();
+            txtNombre.Focus();
+        }
+
+        private string ObtenerTipoDocumento()
+        {
+            if (rdbDni.Checked) return "DNI";
+            else if (rdbLe.Checked) return "LE";
+            else if (rdbLc.Checked) return "LC";
+            else if (rdbPas.Checked) return "PAS";
+            else return "";
+        }
+
+        private string ObtenerGenero()
+        {
+            if (rdbHombre.Checked) return rdbHombre.Text;
+            else if (rdbMujer.Checked) return rdbMujer.Text;
+            else if (rdbNoBinario.Checked) return rdbNoBinario.Text;
+            else if (rdbOtro.Checked) return rdbOtro.Text;
+            else return "";
+        }
+
+        private void chkCovid_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkCovid.Checked) cboCovidDosis.Enabled = true;
+            else
+            {
+                cboCovidDosis.Enabled = false;
+                cboCovidDosis.SelectedIndex = 0;
+            }
+        }
+
+        private void chkDobreBacteriana_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDobreBacteriana.Checked) dtpDobleBacUltimaDosis.Enabled = true;
+            else
+            {
+                dtpDobleBacUltimaDosis.Enabled = false;
+                dtpDobleBacUltimaDosis.Value = DateTime.Now.Date;
+            }
+        }
+
+        private void RestablecerControles ()
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            rdbDni.Checked = true;
+            txtNroDocumento.Clear();
+            dtpFecNacimiento.Value = DateTime.Now.Date;
+            cboLocalidad.SelectedIndex = -1;
+            rdbHombre.Checked = true;
+
+            foreach (Control control in gbxVacunas.Controls)
+            {
+                if (control is CheckBox checkBox)
+                    checkBox.Checked = false;
+            }
+
+            cboCovidDosis.SelectedIndex = 0;
+            cboCovidDosis.Enabled = false;
+            dtpDobleBacUltimaDosis.Value = DateTime.Now.Date;
+            dtpDobleBacUltimaDosis.Enabled = false;
+        }
+    }
+}
---
+++ TPLaboratorioUAI/frmPrincipal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPLaboratorioUAI
+{
+    public partial class frmPrincipal : Form
+    {
+        frmForm1 form1;
+        frmForm2 form2;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+
+            IsMdiContainer = true;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnForm1_Click(object sender, EventArgs e)
+        {
+            if (form2 != null) form2.Close();
+
+            form1 = new frmForm1();
+            form1.MdiParent = this;
+            pnlRight.Controls.Add(form1);
+            form1.WindowState = FormWindowState.Maximized;
+            form1.Show();
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            if (form1 != null) form1.Close();
+
+            form2 = new frmForm2();
+            form2.MdiParent = this;
+            pnlRight.Controls.Add(form2);
+            form2.WindowState = FormWindowState.Maximized;
+            form2.Show();
+        }
+    }
+}
